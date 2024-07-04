3600871400df26e2a6a52d15fc10642e190bf747
Vaibhav Jadhav <vaibhav.jadhav0596@gmail.com>
2024-07-04 21:49:29 +0530

    Done

--- BE/config-server/src/main/java/com/config/server/ConfigServerApplication.java
+++ BE/config-server/src/main/java/com/config/server/ConfigServerApplication.java
@@ -1,9 +1,11 @@
-package com.eureka.server;
+package com.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
