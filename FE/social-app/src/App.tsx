1c38abc82ee90835fa61bf9fd2e0379876b191c5
Vaibhav Jadhav <vaibhav.jadhav0596@gmail.com>
2024-07-12 14:54:57 +0530

    Initial setup

---
+++ FE/social-app/src/App.tsx
@@ -0,0 +1,26 @@
+import "./globals.css";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./_root/pages";
+import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
---
+++ FE/social-app/src/_auth/forms/SignUpForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const SignUpForm = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default SignUpForm;
