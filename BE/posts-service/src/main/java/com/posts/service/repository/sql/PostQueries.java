739a8916b632a2d29b0f2e536b6dbc92c3c058d5
Vaibhav Jadhav <vaibhav-jadhav0512>
2024-07-28 22:39:11 +0530

    createdAT

--- BE/posts-service/src/main/java/com/posts/service/repository/sql/PostQueries.java
+++ BE/posts-service/src/main/java/com/posts/service/repository/sql/PostQueries.java
@@ -7,7 +7,7 @@ public class PostQueries {
 			+ "VALUES(nextval('auth.posts_id_seq'::regclass), :userName, :caption, :location, :tags, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) RETURNING id";
 	public static final String INSERT_FILE = "INSERT INTO auth.files "
 			+ "(id, url, post_id) " + "VALUES(nextval('auth.files_id_seq'::regclass), :url, :postId);";
-	public static final String GET_BY_POST_ID = "SELECT id, user_name, caption, \"location\", tags "
+	public static final String GET_BY_POST_ID = "SELECT id, user_name, caption, \"location\", tags, created_at "
 			+ "FROM auth.posts WHERE id = :postId";
 	public static final String GET_FILES_BY_POST_ID = "SELECT id, url, post_id "
 			+ "FROM auth.files WHERE post_id=:postId";
