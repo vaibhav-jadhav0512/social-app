597a247213eae0dd3e88404b59de6996c39e818f
Vaibhav Jadhav <vaibhav-jadhav0512>
2024-08-02 15:27:20 +0530

    Post

--- BE/user-info-service/src/main/java/com/user/info/service/controller/UserInfoController.java
+++ BE/user-info-service/src/main/java/com/user/info/service/controller/UserInfoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +37,10 @@ public class UserInfoController {
 	@GetMapping("/get/all")
 	public ResponseEntity<List<UserInfoDto>> getAllUsers() {
 		return new ResponseEntity<>(service.getAllUsers(), HttpStatus.OK);
+	}
+
+	@GetMapping("/get/{userName}")
+	public ResponseEntity<UserInfo> getUserInfo(@PathVariable("userName") String userName) {
+		return new ResponseEntity<>(service.getUserInfo(userName), HttpStatus.OK);
 	}
 }
