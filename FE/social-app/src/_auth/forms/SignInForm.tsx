9a2de173a4a4e8cfdcc05e61577fbfdf4a847a3e
Vaibhav Jadhav <vaibhav.jadhav0596@gmail.com>
2024-08-08 02:06:13 +0530

    Test done

--- FE/social-app/src/_auth/forms/SignInForm.tsx
+++ FE/social-app/src/_auth/forms/SignInForm.tsx
@@ -60,10 +60,9 @@ const SigninForm = () => {
 
   return (
     <Form {...form}>
-      <h1>Hello world</h1>
       <div className="sm:w-420 flex-center flex-col">
         <img src="/assets/images/logo.svg" alt="logo" />
-
+        <h1>Developed by Vaibhav Jadhav</h1>
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Log in to your account
         </h2>
