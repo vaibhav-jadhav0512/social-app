7f5129d4574104a43537003a8cef75ca89f12251
Vaibhav Jadhav <vaibhav.jadhav0596@gmail.com>
2024-08-08 12:55:07 +0530

    Posts service deploy

--- BE/posts-service/src/main/java/com/posts/service/controller/SaverController.java
+++ BE/posts-service/src/main/java/com/posts/service/controller/SaverController.java
@@ -20,7 +20,7 @@ import com.posts.service.service.SaveService;
 
 @RestController
 @RequestMapping("/saver")
-@CrossOrigin(origins = "http://65.0.45.60:5173", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}, allowCredentials = "true")
+@CrossOrigin(origins = "http://13.233.60.26:5173", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}, allowCredentials = "true")
 public class SaverController {
 
 	@Autowired
